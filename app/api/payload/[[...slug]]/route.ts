e3f076518fd3e2fb8c0ee2e162e40539497a62f8
Jim Watkins <watkinsllc@gmail.com>
2026-01-15 15:49:53 -0500

    Create route.ts

---
+++ app/api/payload/[[...slug]]/route.ts
@@ -0,0 +1,6 @@
+import { payloadHandler } from '@payloadcms/next/handlers'
+import config from '../../../../payload.config'
+
+export const { GET, POST, PUT, PATCH, DELETE } = payloadHandler({
+  config,
+})
