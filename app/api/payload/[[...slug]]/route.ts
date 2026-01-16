2731fb89df2102c2e506ecdf78f7d32af468210a
pagevalleynews <watkinsllc@gmail.com>
2026-01-16 11:12:07 -0500

    Fix Payload v3 API route

--- app/api/payload/[[...slug]]/route.ts
+++ app/api/payload/[[...slug]]/route.ts
@@ -1,6 +1,27 @@
-import { payloadHandler } from '@payloadcms/next/handlers'
+import { getPayload } from 'payload'
 import config from '../../../../payload.config'
+import { NextRequest } from 'next/server'
 
-export const { GET, POST, PUT, PATCH, DELETE } = payloadHandler({
-  config,
-})
+export const runtime = 'nodejs'
+
+const payload = await getPayload({ config })
+
+export async function GET(req: NextRequest) {
+  return payload.handle(req)
+}
+
+export async function POST(req: NextRequest) {
+  return payload.handle(req)
+}
+
+export async function PUT(req: NextRequest) {
+  return payload.handle(req)
+}
+
+export async function PATCH(req: NextRequest) {
+  return payload.handle(req)
+}
+
+export async function DELETE(req: NextRequest) {
+  return payload.handle(req)
+}
